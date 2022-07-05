1f4115c781da393d1e94a9136774dece44fb13f4
JMComstock <jasoncomstock11@gmail.com>
2022-07-05 15:30:55 -0400

    added exercises to check for time and space complexity

--- exercise.js
+++ exercise.js
@@ -14,21 +14,22 @@
 
 // can be solved with 2 for loops which is O(n^2) which is not very effecient at all - brute force method
 
-// const arr1 = ['a','b','c','x'];
-// const arr2 = ['z','y','x']; 
+const array1 = ['a','b','c','x'];
+const array2 = ['z','y','x']; 
 
-// function containsCommonItem(arr1, arr2) {
-//     for (let i = 0; i < arr1.length; i++) { // O(a)
-//         for (let j = 0; j < arr2.length; j++) { // O(b)
-//             if(arr1[i] === arr2[j]) {
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-// } 
+function containsCommonItem(array1, array2) {
+    for (let i = 0; i < array1.length; i++) { // O(a)
+        for (let j = 0; j < array2.length; j++) { // O(b)
+            if(array1[i] === array2[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+} 
 // O(a*b)
-// console.log(containsCommonItem(arr1, arr2)); // returns true
+// O(1) - Space Complexity
+console.log(containsCommonItem(array1, array2)); // returns true
 
 
 const arr1 = ['a','b','c','x'];
@@ -44,13 +45,29 @@ const arr2 = ['z','y','x'];
 
 function containsCommonItem2(arr1, arr2) {
     // loop through first array and create object where properties === items in the array
-    for (let i = 0; i < arr1.length; i++) {
+    // can we assume always 2 parameters?
 
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = arr1[i];
+            map[item] = true;
+        }
     }
     // loop through second array and check if item in second array exists on created object
     for (let j = 0; j < arr2.length; j++) {
-        
+        if (map[arr2[j]]) {
+            return true;
+        }
     }
+    return false;
 }
-
+containsCommonItem2(arr1, arr2);
 //O(a + b)
+// O(a) Space Complexity
+
+
+function containsCommonItem3 (a1, a2) {
+    return a1.some(item => a2.includes(item));
+}
+containsCommonItem3(arr1, arr2);
